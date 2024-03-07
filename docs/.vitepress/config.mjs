5affe6c91b21d5b533736d283c1384ea1f18ae47
findnr <cym504875043@gmail.com>
2024-03-07 16:51:29 +0800

    frist commit

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,43 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2024-03-07 15:33:13
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2024-03-07 15:40:22
+ * @FilePath: \instance_collection\docs\.vitepress\config.mjs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "收集实战案例，方便大家查阅",
+  description: "收集实战案例，方便大家查阅",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
+  },
+  appearance: true,
+  lang: 'zh-CN',
+  lastUpdated: true
+})
---
+++ docs/index.md
@@ -0,0 +1,26 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "收集分享学习进步"
+  text: ""
+  tagline: My great project tagline
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: ubuntu系统搭建nginx php mysql(mariadb)
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+    link: /nginx/ubuntu/lnmp
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
