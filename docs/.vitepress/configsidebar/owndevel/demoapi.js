3c89783a1d0590840883496bec1271e3f4119bed
findnr <cym504875043@gmail.com>
2024-04-08 16:29:13 +0800

    202404081929

---
+++ docs/.vitepress/configsidebar/owndevel/demoapi.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: findnr
+ * @Date: 2024-04-08 13:55:39
+ * @LastEditors: findnr
+ * @LastEditTime: 2024-04-08 15:31:59
+ * @Description: 
+ */
+export default {
+    "/os/win/": [{
+        text: 'win相关',
+        link: 'index',
+        items: [
+            { text: '常用命令', link: 'commond' },
+            { text: 'win相关产品激活', link: 'activate' },
+            { text: 'win gost安装系统', link: 'gost' },
+        ]
+    }]
+}
