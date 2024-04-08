68fa6885fa6468824334db80c9192bec748dfbf7
findnr <cym504875043@gmail.com>
2024-04-08 16:36:32 +0800

    202404081636

--- docs/.vitepress/configsidebar/owndevel/demoapi.js
+++ docs/.vitepress/configsidebar/owndevel/demoapi.js
@@ -6,13 +6,11 @@
  * @Description: 
  */
 export default {
-    "/os/win/": [{
-        text: 'win相关',
+    "/owndevel/demoapi/": [{
+        text: 'thinkphp_api相关',
         link: 'index',
         items: [
-            { text: '常用命令', link: 'commond' },
-            { text: 'win相关产品激活', link: 'activate' },
-            { text: 'win gost安装系统', link: 'gost' },
+            { text: '安装', link: 'install' },
         ]
     }]
 }
--- docs/.vitepress/configsidebar/owndevel/vue3page.js
+++ docs/.vitepress/configsidebar/owndevel/vue3page.js
@@ -1,11 +1,9 @@
 export default {
-    "/os/win/": [{
-        text: 'win相关',
+    "/owndevel/vue3page/": [{
+        text: 'vue3page相关',
         link: 'index',
         items: [
-            { text: '常用命令', link: 'commond' },
-            { text: 'win相关产品激活', link: 'activate' },
-            { text: 'win gost安装系统', link: 'gost' },
+            { text: '安装', link: 'install' },
         ]
     }]
 }
