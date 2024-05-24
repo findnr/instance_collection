7afadb7c58f4a1ec10ff2aa1d71902b85bb24bfb
findnr <cym504875043@gmail.com>
2024-05-24 10:50:45 +0800

    20240524

--- docs/.vitepress/configsidebar/os/android.js
+++ docs/.vitepress/configsidebar/os/android.js
@@ -5,6 +5,7 @@ export default {
         items: [
             { text: '常用工具集', link: 'tools' },
             { text: '常用设置', link: 'setting' },
+            { text: '使用手机变电脑摄像头', link: 'phonetocam' },
         ]
     }]
 }
