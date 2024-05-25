703ad235c9e215367b6822f705b64deb65c6ed82
findnr <cym504875043@gmail.com>
2024-05-26 06:47:26 +0800

    202405260647

--- docs/.vitepress/configsidebar/os/win.js
+++ docs/.vitepress/configsidebar/os/win.js
@@ -6,6 +6,7 @@ export default {
             { text: '常用命令', link: 'commond' },
             { text: 'win相关产品激活', link: 'activate' },
             { text: 'win gost安装系统', link: 'gost' },
+            { text: '开启多用户远程', link: 'remote' },
         ]
     }]
 }
