b7508f736fd6cfb0c3c7af78b9aabe351f708088
findnr <cym504875043@gmail.com>
2024-06-26 21:02:29 +0800

    202406262102

--- docs/.vitepress/configsidebar/network/zerotier.js
+++ docs/.vitepress/configsidebar/network/zerotier.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: findnr
+ * @Date: 2024-04-03 13:30:11
+ * @LastEditors: findnr
+ * @LastEditTime: 2024-06-26 20:26:50
+ * @Description: 
+ */
 export default {
     "/network/zerotier/": [{
         text: 'zerotier相关信息',
@@ -5,6 +12,7 @@ export default {
         items: [
             { text: 'zerotier安装', link: 'install' },
             { text: '配置 moon 节点加速访问速度', link: 'moon' },
+            { text: '删除', link: 'remove' },
         ]
     }]
 }
--- docs/.vitepress/configsidebar/os/rockylinux.js
+++ docs/.vitepress/configsidebar/os/rockylinux.js
@@ -1,9 +1,16 @@
+/*
+ * @Author: findnr
+ * @Date: 2024-03-14 09:23:32
+ * @LastEditors: findnr
+ * @LastEditTime: 2024-06-26 20:52:10
+ * @Description: 
+ */
 export default {
     "/os/rockylinux/": [{
         text: 'rockylinux相关',
         link: 'index',
         items: [
-            { text: '常用命令', link: 'commond' },
+            { text: '常用命令', link: 'command' },
             { text: 'samba服务配制案例', link: 'samba' },
         ]
     }]
