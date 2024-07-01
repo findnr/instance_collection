dec4370f5df0db2cbe4ea126a82f091c5e6e7a92
findnr <cym504875043@gmail.com>
2024-07-01 11:29:38 +0800

    202407011129

---
+++ docs/.vitepress/configsidebar/os/openwrt.js
@@ -0,0 +1,9 @@
+export default {
+    "/os/openwrt/": [{
+        text: 'openwrt相关',
+        link: 'index',
+        items: [
+            { text: 'VirtualBox安装openwrt', link: 'virtualbox' },
+        ]
+    }]
+}
