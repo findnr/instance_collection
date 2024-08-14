189e2b6da40a3908b0d58ab97fbb55812c965186
findnr <cym504875043@gmail.com>
2024-08-14 11:16:26 +0800

    202408141116

--- docs/.vitepress/configsidebar/development/sql.js
+++ docs/.vitepress/configsidebar/development/sql.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: findnr
+ * @Date: 2024-04-03 14:13:39
+ * @LastEditors: findnr
+ * @LastEditTime: 2024-08-14 11:09:29
+ * @Description: 
+ */
 export default {
     "/development/sql/": [
     {
@@ -5,6 +12,7 @@ export default {
         link: 'mysql',
         items: [
             { text: '安装', link: 'install' },
+            { text: '表数据结构与数据', link: 'copy_table' },
         ]
     },{
         text: 'redis',
